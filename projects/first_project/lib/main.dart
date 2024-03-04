5255e746b974d6a2a76cff93f8c9612e70436cbd
constructorlib <constructorlib@gmail.com>
2024-03-04 11:29:12 +0900

    encountering error with materialapp definition, fix later

--- projects/first_project/lib/main.dart
+++ projects/first_project/lib/main.dart
@@ -10,9 +10,21 @@ import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
+final myTheme = ThemeData(
+  primaryColor: Colors.blue, // Set primary color to blue
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    return MaterialApp(
+      theme: myTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first ever App'),
+        ),
+        body: Text(" lorem ipsum dolor sit amet"),
+      ),
+    );
   }
 }
