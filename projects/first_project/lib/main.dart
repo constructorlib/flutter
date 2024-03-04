6a829169269ead59a68f0f0a644c9e8bbffdb591
constructorlib <constructorlib@gmail.com>
2024-03-04 16:02:18 +0900

    Migrate to Android Studio, use anonymous function in MyApp to define button

--- projects/first_project/lib/main.dart
+++ projects/first_project/lib/main.dart
@@ -8,22 +8,45 @@ import "package:flutter/material.dart";
 //   */
 // }
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 final myTheme = ThemeData(
   primaryColor: Colors.blue, // Set primary color to blue
 );
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  Widget buildButton() {
+    return ElevatedButton(
+      onPressed: () {
+        print('Answer 1 pressed!');
+      },
+      child: Text('Answer 1'),
+    );
+  }
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "'What's your favorite color?'",
+      'How would you describe your mood right now?'
+    ];
     return MaterialApp(
       theme: myTheme,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My first ever App'),
+          title: const Text('My first ever App asdasssd'),
         ),
-        body: Text(" lorem ipsum dolor sit amet"),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: () {
+                print('Answer 1 pressed!');
+              },
+              child: Text('Answer 1'),
+            ),
+          ],
+        ),
       ),
     );
   }
