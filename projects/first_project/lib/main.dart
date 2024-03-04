3c4d51b1b74a27a620733d3980a2f70276fd9c9c
constructorlib <constructorlib@gmail.com>
2024-03-04 10:37:42 +0900

    stateless widget fix using --apply

--- projects/first_project/lib/main.dart
+++ projects/first_project/lib/main.dart
@@ -1,11 +1,14 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello"));
+    return const MaterialApp(home: Text("Hello"));
   }
 }
