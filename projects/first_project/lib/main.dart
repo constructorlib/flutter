fa9030a929f8a954ee8e0d99bd1b7f6021d66465
constructorlib <constructorlib@gmail.com>
2024-05-12 13:12:14 +0900

    yaml assets and png

--- projects/first_project/lib/main.dart
+++ projects/first_project/lib/main.dart
@@ -7,17 +7,16 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey[500],
         appBar: AppBar(
-          title: Center(
+          title: const Center(
               child: Text(
             'I am Rich',
             style: TextStyle(color: Colors.white),
           )),
           backgroundColor: Colors.blueGrey[900],
         ),
-        body: Center(
+        body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://buffer.com/library/content/images/2023/10/free-images.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
