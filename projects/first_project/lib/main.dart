0bc8c39f335d0fbb51926476e45a0b411bbc2640
constructorlib <constructorlib@gmail.com>
2024-05-12 12:28:39 +0900

    centering, parent-child

--- projects/first_project/lib/main.dart
+++ projects/first_project/lib/main.dart
@@ -1,9 +1,26 @@
 import "package:flutter/material.dart";
 
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text("Hello, World"),
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[500],
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            'I am Rich',
+            style: TextStyle(color: Colors.white),
+          )),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://buffer.com/library/content/images/2023/10/free-images.jpg'),
+          ),
         ),
       ),
-    );
+    ),
+  );
+}
