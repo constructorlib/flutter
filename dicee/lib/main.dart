9deb0705e656245853ffacc91202b381ed45d7b1
constructorlib <constructorlib@gmail.com>
2024-05-21 14:20:24 +0900

    FlatButton deprecated & replaced by Text Button

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -23,16 +23,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/dice1.png'),
-            ),
+            child: TextButton(
+                onPressed: () {
+                  return null;
+                },
+                child: Image.asset('images/dice1.png')),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/dice1.png'),
-            ),
+            child: TextButton(
+                onPressed: () {
+                  return null;
+                },
+                child: Image.asset('images/dice1.png')),
           ),
         ],
       ),
