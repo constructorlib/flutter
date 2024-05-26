e0255ca1277d4c83dfcf5e26b1f9378b91849273
constructorlib <constructorlib@gmail.com>
2024-05-26 17:41:41 +0900

    add all notes + (non Material) colors

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -29,16 +29,93 @@ class _XylophoneAppState extends State<XylophoneApp> {
           title: Text('Xylophone App'),
         ),
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(
-                  AssetSource('note1.wav'),
-                );
-              },
-              child: Text('Play me!'),
-            ),
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('note1.wav'),
+                  );
+                },
+                child: Text('Play me!'),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('note2.wav'),
+                  );
+                },
+                child: Text('Play me!'),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('note3.wav'),
+                  );
+                },
+                child: Text('Play me!'),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                ),
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('note4.wav'),
+                  );
+                },
+                child: Text('Play me!'),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('note5.wav'),
+                  );
+                },
+                child: Text('Play me!'),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.brown,
+                ),
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('note6.wav'),
+                  );
+                },
+                child: Text('Play me!'),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blueAccent[100],
+                ),
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('note7.wav'),
+                  );
+                },
+                child: Text('Play me!'),
+              ),
+            ],
           ),
         ),
       ),
